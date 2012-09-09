0bd79e7208ba1b9fd659411b92453155c46aee6a
Asheesh Laroia <asheesh@asheesh.org>
2012-09-09 12:35:14 -0700

    Add tests and fixes so Google Code bug importer can work through scrapy

--- bugimporters/tests/test_google.py
+++ bugimporters/tests/test_google.py
@@ -1,11 +1,12 @@
 import datetime
 import os
 import mock
-import twisted
 
-from bugimporters.tests import (Bug, ReactorManager, TrackerModel,
-        FakeGetPage, ObjectFromDict)
+from bugimporters.tests import Bug, ObjectFromDict
 from bugimporters.google import GoogleBugParser
+import bugimporters.trac
+import bugimporters.main
+import autoresponse
 from mock import Mock
 
 
@@ -48,6 +49,30 @@ class TestGoogleBugImport(object):
     @staticmethod
     def assertEqual(x, y):
         assert x == y
+
+    def test_top_to_bottom(self):
+        spider = bugimporters.main.BugImportSpider()
+        spider.input_data = [dict(
+                    tracker_name='SymPy',
+                    google_name='sympy',
+                    bitesized_type='label',
+                    bitesized_text='EasyToFix',
+                    documentation_type='label',
+                    documentation_text='Documentation',
+                    bugimporter = 'google.GoogleBugImporter',
+                    queries=[
+                    'https://code.google.com/feeds/issues/p/sympy/issues/full?can=open&max-results=10000' +
+                    '&label=EasyToFix']
+                    )]
+        url2filename = {
+            'https://code.google.com/feeds/issues/p/sympy/issues/full?can=open&max-results=10000&label=EasyToFix':
+                os.path.join(HERE, 'sample-data', 'google',
+                             'label-easytofix.atom'),
+            }
+        ar = autoresponse.Autoresponder(url2filename=url2filename,
+                                        url2errors={})
+        items = ar.respond_recursively(spider.start_requests())
+        assert len(items) == 74
 
     def test_create_google_data_dict_with_everything(self):
         atom_dict = {
