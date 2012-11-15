3324056acd778d650cb19f127db1954c09b91651
Asheesh Laroia <asheesh@asheesh.org>
2012-11-15 18:57:29 -0500

    Remove unused imports

--- bugimporters/bugzilla.py
+++ bugimporters/bugzilla.py
@@ -16,10 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
 import lxml
-import twisted.web.error
-import twisted.web.http
 import urlparse
 import logging
 import scrapy
