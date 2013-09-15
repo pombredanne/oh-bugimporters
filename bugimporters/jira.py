576289c9617d6d586f276480e00e822969db5daf
Aaron Hill <aa1ronham@gmail.com>
2013-09-15 15:35:01 -0400

    Initial work

---
+++ bugimporters/jira.py
@@ -0,0 +1,81 @@
+# This file is part of OpenHatc
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import scrapy.http
+import scrapy.spider
+
+import bugimporters.items
+from bugimporters.base import BugImporter, printable_datetime
+from bugimporters.helpers import string2naive_datetime
+
+class JiraBugImporter(BugImporter):
+    def process_queries(self, queries):
+        for query in queries:
+            yield scrapy.http.Request(
+                url=query,
+                callback=self.handle_bug_list_response)
+
+    def handle_bug_list_response(self, response):
+        issue_list = json.loads(response.body)
+
+        for bug in issue_list['issues']:
+            yield self.handle_bug(bug)
+
+
+    def process_bugs(self, bug_list, older_bug_data_url):
+        r = scrapy.http.Request(
+            url=older_bug_data_url,
+            callback=self.handle_old_bug_query)
+        # For historical reasons, bug_list is a tuple of (url, data).
+        # We just want the URLs.
+        r.meta['bug_list'] = [url for (url, data) in bug_list]
+        yield r
+
+    def handle_old_bug_query(self, response):
+        bugs_we_care_about = response.meta['bug_list']
+        bugs_from_response = json.loads(response.body)
+        for bug in bugs_from_response:
+            if bug['self'] in bugs_we_care_about:
+                yield self.handle_bug(bug)
+
+    def handle_bug_show_response(self, response):
+        bug_data = json.loads(response.body)
+        return self.handle_bug(bug_data)
+
+    def handle_bug(self, bug_data):
+        jbp = JiraBugParser(self.tm)
+        return jbp.parse(bug_data)
+
+class JiraBugParser(object):
+    def __init__(self, tm):
+        self.tm = tm
+
+    def parse(self, issue):
+        parsed = bugimporters.items.ParsedBug({
+            'title': issue['fields']['summary'],
+            'description': issue['fields']['description'],
+            'status': issue['fields']['status']['name'],
+            'date_reported': printable_datetime(string2naive_datetime(issue['fields']['created'])),
+            'last_touched': printable_datetime(string2naive_datetime(issue['fields']['updated'])),
+            'submitter_username': issue['fields']['reporter']['name'],
+            'submitter_realname': issue['fields']['reporter']['displayName'],
+            'looks_closed': (issue['fields']['status']['name'] == 'Closed'),
+            'last_polled': printable_datetime(),
+            '_project_name': self.tm.tracker_name,
+            '_tracker_name': self.tm.tracker_name,
+        })
+
+        return parsed
